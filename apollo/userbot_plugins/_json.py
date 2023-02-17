92cb642f9d2d6b090780ad108b4fc9907e9db74b
jwaxy <98110672+jwaxy@users.noreply.github.com>
2023-02-17 13:42:45 +0300

    fix some things & add hg_hb sa_as

--- apollo/userbot_plugins/_json.py
+++ apollo/userbot_plugins/_json.py
@@ -4,8 +4,12 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pathlib import Path
 
+
 @Client.on_message(filters.command(['json'], PREFIX) & filters.me)
-async def jsn_ver(client:Client, message:Message):
+async def jsn_ver(client: Client, message: Message):
     await message.edit(f"```{message.reply_to_message}```")
 
-CmdHelp(Path(__file__).stem).add_command("json", None, "Bir mesajı yanıtlayın ve onun ham halini alın.").add_userbot()
+myCmdHelp = CmdHelp(Path(__file__).stem)
+
+CmdHelp(Path(__file__).stem).add_command("json", None,
+                                         "Bir mesajı yanıtlayın ve onun ham halini alın.").add_userbot()
---
+++ apollo/userbot_plugins/hg_hb.py
@@ -0,0 +1,52 @@
+from apollo.lib.CmdHelp import CmdHelp
+from pathlib import Path
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from asyncio import sleep
+
+DELAY = 0.6
+
+
+@Client.on_message(filters.regex("^Hg$") & filters.outgoing)
+async def hosgeldin(client: Client, message: Message):
+    hg_list = [
+        "**Hoşgeldiniiiiz!**",
+        "**Hg kanka 😳**",
+        "**HG REİSS 😎**",
+        "**Neredesin yav Hoşgeldin! **",
+        "Hoşgeldin 😇",
+        "**HGGGGGGG🍁**",
+        "**Naber hg 🥶**",
+        "**Sonunda Geldin Ha 😉**",
+        "**Hoşgeldin 🔥😜**"
+    ]
+
+    for msg in hg_list:
+        await message.edit(msg)
+        sleep(DELAY)
+
+
+@Client.on_message(filters.regex("^Hb$") & filters.outgoing)
+async def hosbuldum(client: Client, message: Message):
+    hb_list = [
+        "**Hoşşbulldumm 😊**",
+        "**Hb hb kanka 😳**",
+        "**Geldik reis 😎**",
+        "**Buradayızzz hoşbulduk 😏**",
+        "Hoşbuldum 😇",
+        "**Hoşbulduk hoşgördük 🤠🤠🤠**",
+        "**Hepinize hb🥶**",
+        "**Hb 😴**",
+        "**Hoşbuldum 🙂**"
+    ]
+
+    for msg in hb_list:
+        await message.edit(msg)
+        sleep(DELAY)
+
+myCmdHelp = CmdHelp(Path(__file__).stem)
+
+myCmdHelp.add_command("Hg", None,
+                      "Havalı bir şekilde Hoş geldin yazar.", is_cmd=False).add_userbot()
+myCmdHelp.add_command("Hb", None,
+                      "Havalı bir şekilde Hoş buldum yazar.", is_cmd=False).add_userbot()
