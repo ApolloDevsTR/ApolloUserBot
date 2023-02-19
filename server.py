cfa3cd91a10d30d9d7a7b6326685202773dc09e4
jwaxy <98110672+jwaxy@users.noreply.github.com>
2023-02-19 16:14:51 +0300

    hmm

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
-import os
 from flask import Flask
-from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
-print("Starting web server..")
+@app.route('/')
+def hello_world():
+    return "Apollo Userbot Çalışıyor!"
 
-class Selamla(Resource):
-    def get(self):
-        return "Apollo Userbot Çalışıyor!"
-
-
-api.add_resource(Selamla, '/')
-app.run(host="0.0.0.0", port=10000)
+if __name__ == '__main__':
+    app.run()
